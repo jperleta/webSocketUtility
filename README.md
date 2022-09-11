89b5de5708244d2edf77b8eda0302ebde5b27f33
Jerko Perleta <jperleta@gmail.com>
2022-09-11 14:04:31 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# webSocketUtility
+# WebSocketUtility
 Small utility that supports native web socket connection added in iOS 13.
+
+A WebSocket is a network protocol that allows two-way communication between a server and client. Unlike HTTP, which uses a request and response pattern, WebSocket peers can send messages in either direction at any point in time. WebSockets are often used for chat-based apps and other apps that need to continuously talk between server and client.
+
+This utility supports:
+- Initiating a WebSocket connection.
+- Disconnecting from WebSocket session.
+- Sending String or Data messages through the WebSocket.
+- Processing received messages sent through the WebSocket.
---
+++ SocketInvoker.swift
@@ -0,0 +1,116 @@
+import Foundation
+
+protocol WebSocketConnection {
+    /// Opening web socket connection
+    func connect()
+    
+    /// Closing web socket connection
+    func disconnect()
+    
+    /// For sending a message of type String
+    func send(text: String)
+    
+    /// For sending a message of type Data
+    func send(data: Data)
+}
+
+protocol WebSocketConnectionDelegate: AnyObject {
+    /// On connection opened
+    func onConnected(connection: WebSocketConnection)
+    
+    /// On connection closed
+    func onDisconnected(connection: WebSocketConnection, error: Error?)
+    
+    /// On error received
+    func onError(connection: WebSocketConnection, error: Error)
+    
+    /// On String message received
+    func onMessage(connection: WebSocketConnection, text: String)
+    
+    /// On Data message recived
+    func onMessage(connection: WebSocketConnection, data: Data)
+}
+
+class SocketInvoker: NSObject, WebSocketConnection {
+    private var session: URLSession?
+    private var webSocketTask: URLSessionWebSocketTask?
+    private weak var delegate: WebSocketConnectionDelegate?
+    
+    init(url: URL, delegate: WebSocketConnectionDelegate) {
+        super.init()
+        self.session = URLSession(configuration: .default, delegate: self, delegateQueue: OperationQueue())
+        self.webSocketTask = session?.webSocketTask(with: url)
+        self.delegate = delegate
+    }
+    
+    func connect() {
+        webSocketTask?.resume()
+        
+        listen()
+        keepConnectionLive()
+    }
+    
+    func disconnect() {
+        webSocketTask?.cancel(with: .goingAway, reason: nil)
+    }
+    
+    func send(text: String) {
+        webSocketTask?.send(URLSessionWebSocketTask.Message.string(text)) { error in
+            if let error = error {
+                self.delegate?.onError(connection: self, error: error)
+            }
+        }
+    }
+    
+    func send(data: Data) {
+        webSocketTask?.send(URLSessionWebSocketTask.Message.data(data)) { error in
+            if let error = error {
+                self.delegate?.onError(connection: self, error: error)
+            }
+        }
+    }
+    
+    /// Receive method is only called once. If we want to receive another message, we need to call receive again.
+    private func listen()  {
+        webSocketTask?.receive { result in
+            switch result {
+            case .failure(let error):
+                self.delegate?.onError(connection: self, error: error)
+            case .success(let message):
+                switch message {
+                case .string(let text):
+                    self.delegate?.onMessage(connection: self, text: text)
+                case .data(let data):
+                    self.delegate?.onMessage(connection: self, data: data)
+                @unknown default:
+                    break
+                }
+                
+                self.listen()
+            }
+        }
+    }
+    
+    /// To avoid that server drops connection we use special ping-pong messages. We should send them periodically (approximately every 10 seconds) to make sure the connection won't get killed by the server.
+    private func keepConnectionLive() {
+        webSocketTask?.sendPing { (error) in
+            if let error = error {
+                print("Sending PING failed: \(error)")
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
+                self.keepConnectionLive()
+            }
+        }
+    }
+}
+
+extension SocketInvoker: URLSessionWebSocketDelegate {
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didOpenWithProtocol protocol: String?) {
+        self.delegate?.onConnected(connection: self)
+    }
+
+    func urlSession(_ session: URLSession, webSocketTask: URLSessionWebSocketTask, didCloseWith closeCode: URLSessionWebSocketTask.CloseCode, reason: Data?) {
+        self.delegate?.onDisconnected(connection: self, error: nil)
+    }
+}
